25099b5d87c6dbf3fe4d05dd30c4445f6ea2d85e
jdsi23 <jameslusk535@gmail.com>
2025-06-24 20:08:29 +0000

    POS

--- pos-system/run.sh
+++ pos-system/run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
+echo "[+] Downloading Terraform 1.7.5..."
+curl -O https://releases.hashicorp.com/terraform/1.7.5/terraform_1.7.5_linux_amd64.zip
+
+echo "[+] Unzipping Terraform..."
+unzip -o terraform_1.7.5_linux_amd64.zip
+
+echo "[+] Moving Terraform binary to /usr/local/bin..."
+sudo mv terraform /usr/local/bin/
+
+echo "[+] Making setup.sh and deploy.sh executable..."
+chmod +x run.sh
+
 declare -A regions_ports=(
   ["central"]=5000
   ["east"]=5001
